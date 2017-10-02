83c707bd079101f6463a91ffc6ccee11e46aa903
Adam Titus and Alan Falon <adam+alan@>
2017-10-02 11:58:06 -0700

    Fix bugs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
+function Game() {
+  this.round = new Round();
+}
 
+function Round() {
+  this.screen = new Screen();
+}
+
+function Screen() {
+  const WIDTH = 10;
+  const HEIGHT = 20
+  // New empty board
+  this.blocks = [];
+  for (var i = 0; i < HEIGHT; i++) {
+    this.blocks[i] = [];
+    for (var j = 0; j < WIDTH; j++) {
+      this.blocks[i][j] = null;
+    }
+  }
+}
+
+Screen.prototype.spawnPiece = function (x, y) {
+  // TODO: Error checking for in-bounds
+  this.blocks[y][x] = new Block();
+};
+
+function Piece() {
+
+}
+
+function Block() {
+
+}
+
+// Start game
+$(function(){
+  var game = new Game();
+  $("#start").click(function(){
+    $(".start-menu").hide();
+    $(".board").show();
+  });
+});
