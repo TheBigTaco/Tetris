5d30294455745528439ba5ff32f09ca77804e695
Rane Fields <rane.fields@gmail.com>
2017-10-02 10:28:45 -0700

    Add some comments to main objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
+// Handles loading of the game/menus/etc
 function Game() {
   this.round = new Round();
 }
 
+// Handles actual gameplay logic
 function Round() {
   this.screen = new Screen();
 }
 
+// Tracks position of blocks
 function Screen() {
   this.squares = [];
   for (var i = 0; i < 20; i++) {
@@ -16,13 +19,15 @@ function Screen() {
   }
 }
 
+// Collection of blocks in a particular shape
 function Piece() {
 
 }
 
+// Single square of the board
 function Block() {
 
 }
 
-
+// Start game
 var game = new Game();
